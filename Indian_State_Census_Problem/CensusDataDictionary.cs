cf6ab793c102c8ae5fc48ed0844ae9e7d84018c9
Satantra Tewari <satantra1508@gmail.com>
2020-11-06 00:42:06 +0530

    [satantra] add. dictionary classa added

--- Indian_State_Census_Problem/CensusDataDictionary.cs
+++ Indian_State_Census_Problem/CensusDataDictionary.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Indian_State_Census_Problem.DTO;
+using System.IO;
 using System.Collections.Generic;
-using System.Text;
+using CsvHelper;
+using System.Globalization;
+using System.Linq;
 
 namespace Indian_State_Census_Problem
 {
-    class CensusDataDictionary
+    public class CensusDataDictionary
     {
+        public Dictionary<string, CensusDTO> censusDataMap;
+        public Dictionary<string, CensusDTO> LoadDictionary(string path, StateCensusAnalyser.Country country, string header)
+        {
+            if (country == StateCensusAnalyser.Country.INDIA)            
+                censusDataMap = new IndianCensusDictionary().GetDictionary(path, header);            
+            return censusDataMap;
+        }
     }
 }
