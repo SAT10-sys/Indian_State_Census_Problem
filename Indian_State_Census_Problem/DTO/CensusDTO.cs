96e60c831eca441093b5ab067cfb48afa88ccee0
Satantra Tewari <satantra1508@gmail.com>
2020-11-06 01:01:36 +0530

    [satantra] add. census data to state dictionary

--- Indian_State_Census_Problem/DTO/CensusDTO.cs
+++ Indian_State_Census_Problem/DTO/CensusDTO.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Indian_State_Census_Problem.POCO;
 
 namespace Indian_State_Census_Problem.DTO
 {
-    class CensusDTO
+    public class CensusDTO
     {
+        public int srNo;
+        public string stateName;
+        public int tin;
+        public string stateCode;
+        public string state;
+        public long population;
+        public long area;
+        public long density;
+        public CensusDTO(StateCodeDAO stateCodeDAO)
+        {
+            this.srNo = stateCodeDAO.srNo;
+            this.stateName = stateCodeDAO.stateName;
+            this.tin = stateCodeDAO.tin;
+            this.stateCode = stateCodeDAO.stateCode;
+        }
+        public CensusDTO(CensusDataDAO censusDataDAO)
+        {
+            this.state = censusDataDAO.state;
+            this.population = censusDataDAO.population;
+            this.area = censusDataDAO.area;
+            this.density = censusDataDAO.density;
+        }
     }
 }
--- Indian_State_Census_Problem/POCO/StateCodeDAO.cs
+++ Indian_State_Census_Problem/POCO/StateCodeDAO.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace Indian_State_Census_Problem.POCO
 {
-    class StateCodeDAO
+    public class StateCodeDAO
     {
+        public int srNo;
+        public string stateName;
+        public int tin;
+        public string stateCode;
+        public StateCodeDAO(string srNo, string stateName, string tin, string stateCode)
+        {
+            this.srNo = Convert.ToInt32(srNo);
+            this.stateName = stateName;
+            this.tin = Convert.ToInt32(tin);
+            this.stateCode = stateCode;
+        }
     }
 }
