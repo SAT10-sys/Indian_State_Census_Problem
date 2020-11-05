78e7ca056646ca3297f2dacda82a2491ca378c23
Satantra Tewari <satantra1508@gmail.com>
2020-11-06 00:37:18 +0530

    [satantra] add. exception types stored in enum

---
+++ Indian_State_Census_Problem/CensusAnalyserException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indian_State_Census_Problem
+{
+    public class CensusAnalyserException:Exception
+    {
+        public enum ExceptionType
+        {
+            FILE_NOT_FOUND,
+            INVALID_FILE_TYPE,
+            INCORRECT_DELIMITER,
+            INCORRECT_HEADER,
+            NO_SUCH_COUNTRY
+        }
+        private ExceptionType exceptionType;
+        public CensusAnalyserException(string message, ExceptionType exceptionType):base(message)
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+}
---
+++ Indian_State_Census_Problem/IndianCensusDictionary.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indian_State_Census_Problem
+{
+    class IndianCensusDictionary
+    {
+    }
+}
