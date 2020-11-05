ed41f21a7643c0de48cb3eca1cb62c965b10ba73
Satantra Tewari <satantra1508@gmail.com>
2020-11-06 00:44:37 +0530

    [satantra] add. file exceptions thrown

--- Indian_State_Census_Problem/CheckFileException.cs
+++ Indian_State_Census_Problem/CheckFileException.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
 
 namespace Indian_State_Census_Problem
 {
-    class CheckFileException
+    public class CheckFileException
     {
+        public static void CheckExceptions(string csvPath, StateCensusAnalyser.Country country)
+        {
+            if (File.Exists(csvPath) == false)            
+                throw new CensusAnalyserException("File Does Not Exist", CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);            
+            if (Path.GetExtension(csvPath) != ".csv")           
+                throw new CensusAnalyserException("File Path is Improper", CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE);            
+            if (country != StateCensusAnalyser.Country.INDIA)            
+                throw new CensusAnalyserException("No such country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);            
+        }
     }
 }
