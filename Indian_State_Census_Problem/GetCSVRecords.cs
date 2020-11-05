e2678b79852ff26832db074840bded54b7031017
Satantra Tewari <satantra1508@gmail.com>
2020-11-06 00:47:17 +0530

    [satantra] add. records array added to conatin file path and header

--- Indian_State_Census_Problem/GetCSVRecords.cs
+++ Indian_State_Census_Problem/GetCSVRecords.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
 
 namespace Indian_State_Census_Problem
 {
-    class GetCSVRecords
+    public class GetCSVRecords
     {
+        string[] records;
+        public string[] GetRecords(string path, string header)
+        {
+            records = File.ReadAllLines(path);
+            if(records[0]!=header)
+                throw new CensusAnalyserException("Header is Invalid", CensusAnalyserException.ExceptionType.INCORRECT_HEADER);
+            return records;
+        }
     }
 }
